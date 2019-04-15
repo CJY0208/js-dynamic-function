bcf16448096535e2d02cc44005386f2ac4e1bd4a
CJY <375564567@qq.com>
2019-04-15 15:01:16 +0800

    完善 README、测试用例

--- README.md
+++ README.md
@@ -4,11 +4,23 @@
 
 - - -
 
-## 定义
+## 简介
 
-动态函数是一种特殊的函数，在函数执行前，需要先获取函数执行体，如果获得的执行体为非函数，则动态函数将不执行
+### 定义
+
+动态函数是一种特殊的函数，在函数执行前，需要先调用 `getExecutor` 方法获取函数执行体，如果获得的执行体为非函数，则动态函数将不执行
 
 同时，动态函数拥有一个方法 `isExecutable` 用以检测动态函数是否可以被执行
+
+### 声明方式
+
+```javascript
+new DynamicFunction(getExecutor)
+// or
+new DynamicFunction(name, getExecutor)
+// or
+new DynamicFunction({ name, getExecutor })
+```
 
 - - -
 
@@ -37,7 +49,6 @@ fn.isExecutable() // false
 fn(1, 2) // 无反应
 ```
 
-
 - - -
 
 ## 异步
@@ -46,9 +57,9 @@ fn(1, 2) // 无反应
 
 ```javascript
 
-const delay = time => new Promise(resolve => setTimeout(resolve, time))
+var delay = time => new Promise(resolve => setTimeout(resolve, time))
 
-const fn = new DynamicFunction(async () => {
+var fn = new DynamicFunction(async () => {
   await delay(1000)
 
   return function test(a, b) {
@@ -60,4 +71,47 @@ await fn(1, 2) // log '3' after 1000 ms
 await fn.isExecutable() // log 'true' after 1000 ms
 await fn.getName() // log 'test' after 1000 ms
 await fn.getLength() // log '2' after 1000 ms
+```
+
+- - -
+
+## 其他
+
+### 名字
+
+显式声明函数名
+
+```javascript
+var fn = new DynamicFunction('test', () => {...})
+
+// or
+
+var fn = new DynamicFunction({
+  name: 'test', 
+  getExecutor: () => {...}
+})
+
+fn.getName() // test
+```
+
+若未显式声明函数名，则使用 `executor` 的名称
+
+```javascript
+var fn = new DynamicFunction(() => {
+  return function test() {...}
+})
+
+fn.getName() // test
+```
+
+### 参数个数 / 长度
+
+无法显式声明，使用 `executor` 的长度
+
+```javascript
+var fn = new DynamicFunction(() => {
+  return function test(a, b) {...}
+})
+
+fn.getLength() // 2
 ```
---
+++ test/index.test.js
@@ -0,0 +1,106 @@
+const { assert } = require('chai')
+const DynamicFunction = require('../index')
+
+describe('DynamicFunction 功能测试', () => {
+  it('README 示例', () => {
+    let flag = false
+
+    let getExecutorTimes = 0
+    let runTimes = 0
+
+    const fn = new DynamicFunction(() => {
+      getExecutorTimes++
+      if (!flag) {
+        return null
+      }
+
+      return (a, b) => {
+        runTimes++
+        return a + b
+      }
+    })
+
+    assert.equal(fn.isExecutable(), false) // false
+    assert.equal(getExecutorTimes, 1)
+    assert.equal(runTimes, 0)
+    assert.equal(fn(1, 2), undefined) // 无反应
+    assert.equal(getExecutorTimes, 2)
+    assert.equal(runTimes, 0)
+
+    flag = true
+    assert.equal(fn.isExecutable(), true) // true
+    assert.equal(getExecutorTimes, 3)
+    assert.equal(runTimes, 0)
+    assert.equal(fn(1, 2), 3) // 3
+    assert.equal(getExecutorTimes, 4)
+    assert.equal(runTimes, 1)
+
+    flag = false
+    assert.equal(fn.isExecutable(), false) // false
+    assert.equal(getExecutorTimes, 5)
+    assert.equal(runTimes, 1)
+    assert.equal(fn(1, 2), undefined) // 无反应
+    assert.equal(getExecutorTimes, 6)
+    assert.equal(runTimes, 1)
+  })
+
+  it('异步', async () => {
+    const delay = time => new Promise(resolve => setTimeout(resolve, time))
+
+    const fn = new DynamicFunction(async () => {
+      await delay(1000)
+
+      return function test(a, b) {
+        return a + b
+      }
+    })
+
+    assert.equal(
+      JSON.stringify(
+        await Promise.all([
+          fn(1, 2), // log '3' after 1000 ms
+          fn.isExecutable(), // log 'true' after 1000 ms
+          fn.getName(), // log 'test' after 1000 ms
+          fn.getLength() // log '2' after 1000 ms
+        ])
+      ),
+      JSON.stringify([3, true, 'test', 2])
+    )
+  })
+
+  it('其他 / 名字', () => {
+
+    let fn
+    fn = new DynamicFunction('test', () => undefined)
+
+    assert.equal(fn.getName(), 'test') // test
+
+    // or
+
+    fn = new DynamicFunction({
+      name: 'test',
+      getExecutor: () => undefined
+    })
+
+    assert.equal(fn.getName(), 'test') // test
+
+    fn = new DynamicFunction(() => {
+      return function test() {
+        // nothing
+      }
+    })
+
+    assert.equal(fn.getName(), 'test') // test
+  })
+
+  it('其他 / 长度', () => {
+
+    let fn = new DynamicFunction(() => {
+      return function test(a, b) {
+        // nothing
+      }
+    })
+
+    assert.equal(fn.getLength(), 2) // 2
+  })
+})
